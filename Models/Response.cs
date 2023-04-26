acde3cfcb7f02e85e87483fd77470e3745d12211
George Sucuzhanay <georgesucuzhanay100@gmail.com>
2023-04-25 23:28:47 -0400

    Add files via upload

---
+++ Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace BookstoreAPI.Models
+{
+	public class Response
+	{
+		public int StatusCode { get; set; }
+		public string ? StatusDescription { get; set; }
+		public List<Order> Orders { get; set; } = new List<Order>();
+	}
+}
+
